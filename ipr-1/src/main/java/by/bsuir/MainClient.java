6834fdc40b9d231021c70791a86b3072337642dd
Klezovich <olegolegolegoleg88@gmail.com>
2024-09-25 22:18:32 +0300

    feat(ipr-1): added sockets

---
+++ ipr-1/src/main/java/by/bsuir/MainClient.java
@@ -0,0 +1,40 @@
+package by.bsuir;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/*
+* Задание: Реализовать клиент-серверное приложение и использованием технологии Sockets.
+* Общие требования к заданию:
+* · Клиент может запросить сервер распарсить xml-файл одним из трех видов парсеров: SAX, StAX, DOM.
+* · Сервер производит анализ xml-документа (соответствующим видом парсера) и отправляет
+* пользователю ответ в виде сформированной коллекции объектов.
+* · Запрос клиента и ответ сервера посылаются через сокет в сериализованном виде.
+* · При получении распаршенной информации клиент восстанавливает данные (десериализует их)
+* и выводит в консоль клиента.
+* · Xml-файл хранится на сервере, для валидации xml-файла необходимо разработать
+* соответствующую xsd-схему.
+* · * Сервер также позволяет использовать для анализа xml-документа парсер JDOM.
+* · **Клиент может отправлять запрос не только о предоставлении информации, но и
+* модификации ее (с использованием DOM-парсера).
+*/
+public class MainClient {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8085;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+        {
+            out.println("shufladka");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
