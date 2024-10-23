12c7925aa000ef5cfbbd8ee4befa74553cc1e922
Klezovich <olegolegolegoleg88@gmail.com>
2024-10-23 23:37:44 +0300

    master: deleted ipr-1 module as deprecated

--- ipr-1/src/main/java/by/bsuir/service/MenuService.java
+++
@@ -1,8 +0,0 @@
-package by.bsuir.service;
-
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-
-public interface MenuService {
-    void showMainMenu(BufferedReader in, PrintWriter out);
-}
