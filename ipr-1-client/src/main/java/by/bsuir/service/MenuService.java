cc8de52a5995831168176111b9a5bddedd861918
Klezovich <olegolegolegoleg88@gmail.com>
2024-10-23 23:13:05 +0300

    feat(ipr-1-client): initial commit

---
+++ ipr-1-client/src/main/java/by/bsuir/service/MenuService.java
@@ -0,0 +1,8 @@
+package by.bsuir.service;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public interface MenuService {
+    void showMainMenu(BufferedReader in, PrintWriter out);
+}
